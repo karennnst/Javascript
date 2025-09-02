d36d21929225ada252360471333440fdbefd26d6
Karen Santos <emailkarenescola@gmail.com>
2025-09-02 10:02:54 -0300

    versão módulos OS e FS

---
+++ nodejs/modulos/os/app.js
@@ -0,0 +1,4 @@
+// esse modulo já vem instalado com o Node.js
+// o os (operating system. sistema operacional) fornece informações sobre o sistema operacional
+const os = require('os');
+console.log(os.platform());
